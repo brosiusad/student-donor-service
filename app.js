dedccd6cf2c8dbbb38002840196dfbc1d8b278f8
Artie Brosius <abrosius@soliantconsulting.com>
2014-07-05 18:01:02 -0500

    - initialized express correctly

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 // PostgreSQL setup and connection
 var pg = require('pg');
@@ -11,3 +12,17 @@ client.connect(function(err) {
     }
     console.log('connected!');
 });
+
+app.all('*', function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Origin, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Content-Type", "application/json");
+  next();
+});
+
+// Routes
+app.get('/', function(req, res) {res.send('testing')});
+
+
+app.listen(port);
