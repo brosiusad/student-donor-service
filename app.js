bfcc59a28a6849b92eb55e95a0030cf22283c4d1
Artie Brosius <abrosius@soliantconsulting.com>
2014-07-25 10:52:53 -0500

    - change local database to "Student-Donor" - add more functions to interact with postgresql

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 3000;
 
 // PostgreSQL setup and connection
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://abrosius:@localhost:5432/abrosius';
+var connectionString = process.env.DATABASE_URL || 'postgres://abrosius:@localhost:5432/Student-Donor';
 var client = new pg.Client(connectionString);
 client.connect(function(err) {
     if (err) {
@@ -12,6 +12,71 @@ client.connect(function(err) {
     }
     console.log('connected!');
 });
+
+/* STUDENTS */
+
+var listStudents = function(req, res){
+    var row = client.query('SELECT * FROM student', function (err, result) {
+        if (err) {
+            res.send(404);
+        } else {
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+};
+
+var getStudent = function(req, res) {
+    var row = client.query('SELECT * FROM student WHERE id = $1', req.params.id, function (err, result) {
+        if (err) {
+            res.send(404);
+        } else {
+            res.send(JSON.stringify(result.rows[0]));
+        }
+    });
+
+};
+var deleteStudent = function(req, res){};
+var createStudent = function(req, res){};
+var updateStudent = function(req, res){
+    console.log(req.body);
+};
+
+/* DONORS */
+
+var listDonors = function(req, res){};
+
+var getDonor = function(req, res) {
+    var row = client.query('SELECT * FROM donor WHERE id = $1', req.params.id, function (err, result) {
+        if (err) {
+            res.send(404);
+        } else {
+            res.send(JSON.stringify(result.rows[0]));
+        }
+    });
+
+};
+var deleteDonor = function(req, res){};
+var createDonor = function(req, res){};
+var updateDonor = function(req, res){};
+
+/* TRIPS */
+
+var listTrips = function(req, res){};
+
+var getTrip = function(req, res) {
+    var row = client.query('SELECT * FROM trip WHERE id = $1', req.params.id, function (err, result) {
+        if (err) {
+            res.send(404);
+        } else {
+            res.send(JSON.stringify(result.rows[0]));
+        }
+    });
+
+};
+var deleteTrip = function(req, res){};
+var createTrip = function(req, res){};
+var updateTrip = function(req, res){};
+
 
 // express setup
 app.all('*', function(req, res, next){
@@ -25,16 +90,34 @@ app.all('*', function(req, res, next){
 // Routes
 app.get('/', function(req, res) {
     var query = client.query('SELECT * FROM Student');
-    query.on('row', function(result) {
-        console.log(result);
+    query.on('row', function(row) {
+        console.log(row);
 
-        if (!result) {
+        if (!row) {
           return res.send('No data found');
         } else {
-          res.send('lastname' + result.lastname);
+          res.send('lastname' + row.lastname);
         }
     });
 });
+
+app.get('/students', listStudents);
+app.get('/student/:id', getStudent);
+app.delete('/students/:id', deleteStudent);
+app.post('/students', createStudent);
+app.put('/students/:id', updateStudent);
+
+app.get('/donors', listDonors);
+app.get('/donors/:id', getDonor);
+app.delete('/donors/:id', deleteDonor);
+app.post('/donors', createDonor);
+app.put('/donors/:id', updateDonor);
+
+app.get('/trips', listTrips);
+app.get('/trips/:id', getTrip);
+app.delete('/trips/:id', deleteTrip);
+app.post('/trips', createTrip);
+app.put('/trips/:id', updateTrip);
 
 
 app.listen(port);
