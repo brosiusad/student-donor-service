4793bde62bee65a4d32941e36e55c18bbd604934
Artie Brosius <brosiusad@gmail.com>
2017-08-05 10:29:19 -0500

    update node_modules exclude from git

--- dbcreate.js
+++ dbcreate.js
@@ -1,55 +1,60 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/abrosius';
-console.log(connectionString);
+var config = {
+    user: 'postgres',
+    password: 'postgres',
+    database: 'Student-Donor',
+    port: 5432,
+    host: 'localhost'
+}
 
 var createStudentTableQuery =
-    'CREATE TABLE Student (
-        id SERIAL PRIMARY KEY,
-        firstname text,
-        lastname text NOT NULL,
-        street text,
-        city text,
-        state text,
-        zip text,
-        age integer
-    )';
+    'CREATE TABLE Student (' +
+    '    id SERIAL PRIMARY KEY,' +
+    '    firstname text,' +
+    '    lastname text NOT NULL,' +
+    '    street text,' +
+    '    city text,' +
+    '    state text,' +
+    '    zip text,' +
+    '    age integer' +
+    ')';
 
 var createDonorTableQuery =
-    'CREATE TABLE Donor (
-        id SERIAL PRIMARY KEY,
-        firstname text,
-        lastname text NOT NULL,
-        street text,
-        city text,
-        state text,
-        zip text
-    )';
+    'CREATE TABLE Donor (' +
+    '    id SERIAL PRIMARY KEY,' +
+    '    firstname text,' +
+    '    lastname text NOT NULL,' +
+    '    street text,' +
+    '    city text,' +
+    '    state text,' +
+    '    zip text' +
+    ')';
 
 var createTripTableQuery =
-    'CREATE TABLE Trip (
-        id SERIAL PRIMARY KEY,
-        name text,
-        start_date date,
-        end_date date
-    )';
+    'CREATE TABLE Trip (' +
+    '    id SERIAL PRIMARY KEY,' +
+    '    name text,' +
+    '    start_date date,' +
+    '    end_date date' +
+    ')';
 
 var createTripAttendanceTableQuery =
-    'CREATE TABLE TripAttendance (
-        id SERIAL PRIMARY KEY,
-        student_id integer REFERENCES Student (id) ON DELETE CASCADE,
-        trip_id integer REFERENCES Trip (id) ON DELETE CASCADE
-    )';
+    'CREATE TABLE TripAttendance (' +
+    '    id SERIAL PRIMARY KEY,' +
+    '    student_id integer REFERENCES Student (id) ON DELETE CASCADE,' +
+    '    trip_id integer REFERENCES Trip (id) ON DELETE CASCADE' +
+    ')';
 
 var createDonationTableQuery =
-    'CREATE TABLE Donation (
-        id SERIAL PRIMARY KEY,
-        donor_id integer REFERENCES Donor (id) ON DELETE CASCADE,
-        trip_attendance_id integer REFERENCES TripAttendance (id) ON DELETE CASCADE,
-        amount NUMERIC(7,2)
-    )';
+    'CREATE TABLE Donation (' +
+    '    id SERIAL PRIMARY KEY,' +
+    '    donor_id integer REFERENCES Donor (id) ON DELETE CASCADE,' +
+    '    trip_attendance_id integer REFERENCES TripAttendance (id) ON DELETE CASCADE,' +
+    '    amount NUMERIC(7,2)' +
+    ')';
 
 
-var client = new pg.Client(connectionString);
+var client = new pg.Client(config);
 client.connect();
 
 client.query(createStudentTableQuery);
